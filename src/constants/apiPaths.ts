6f9389b575ed836fc9250a3283c22fa70963fbdc
roboticstechnology <roboticstechnology@gmail.com>
2021-09-26 09:55:11 +0300

    fix:fix url to back

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://cfi6xrxjvc.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://cfi6xrxjvc.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://wq9wrdu5e5.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://wq9wrdu5e5.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://wq9wrdu5e5.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://wq9wrdu5e5.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://wq9wrdu5e5.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
