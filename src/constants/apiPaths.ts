52bf6c647acb63acde12ce466218290ce31499dc
roboticstechnology <roboticstechnology@gmail.com>
2021-10-09 17:46:48 +0300

    feat: chande API imporu file

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://wq9wrdu5e5.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://wq9wrdu5e5.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://wq9wrdu5e5.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://fdvs855zd4.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://wq9wrdu5e5.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://wq9wrdu5e5.execute-api.eu-west-1.amazonaws.com/dev',
 };
