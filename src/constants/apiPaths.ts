ffca93fdeee1baf3e232edc455612cf7728048a0
roboticstechnology <roboticstechnology@gmail.com>
2021-11-01 16:53:43 +0300

    feat: add point to cart API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://wq9wrdu5e5.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://fdvs855zd4.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://wq9wrdu5e5.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://wq9wrdu5e5.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://roboticstechnology-cart-api-dev2.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
